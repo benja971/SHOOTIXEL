38ab946c645c70bc6c561d5b2358ed4259b0166d
Benjamin Peronne <aymerick-971@hotmail.fr>
2020-11-17 09:29:34 -0400

    Rectification du matin

--- Class.py
+++ Class.py
@@ -2,238 +2,244 @@ import pygame
 from random import choice, randint
 from math import cos, sin
 
+
 class ElementGraphique:
-	"""
-	Tout est élément graphique
-	"""
-	def __init__(self, x, y, img, fenetre):
-		self.image = img
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-		self.fenetre = fenetre
+    """
+    Tout est élément graphique
+    """
 
-	def Collide(self, other):
-		if self.rect.colliderect(other.rect):
-			return True
-		return False
+    def __init__(self, x, y, img, fenetre):
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.fenetre = fenetre
 
-	def Collisions(self, other):
-		"""
-		Fonction que gère lorsqu'un élément en touche un autre
-		"""
-		if self.Collide(other):
+    def Collide(self, other):
+        if self.rect.colliderect(other.rect):
+            return True
+        return False
 
-			if self.type == "Enemy" or self.type == "Boss" or self.type == "bonus":
-				if other.type == "TirPerso":
-					self.TakeDamages(other)
-					other.Kill()
+    def Collisions(self, other):
+        """
+        Fonction que gère lorsqu'un élément en touche un autre
+        """
+        if self.Collide(other):
 
-				elif other.type == "Perso":
-					other.TakeDamages(self)
-					self.Kill()
+            if self.type == "Enemy" or self.type == "Boss" or self.type == "bonus":
+                if other.type == "TirPerso":
+                    self.TakeDamages(other)
+                    other.Kill()
 
-	def TakeDamages(self, other):
-		"""
-		Fonction qui retire de la vie à un élément en cas de collision
-		"""
-		self.vie -= other.degats
+                elif other.type == "Perso":
+                    other.TakeDamages(self)
+                    self.Kill()
 
-	def Kill(self):
-		"""
-		Fonction qui tue un élément
-		"""
-		self.vie = 0
+    def TakeDamages(self, other):
+        """
+        Fonction qui retire de la vie à un élément en cas de collision
+        """
+        self.vie -= other.degats
 
-	def Remove(self, list_conscernee):
-		"""
-		Fonction qui supprime les éléments qui n'ont plus lieu d'être
-		"""
-		if self.vie <= 0 and self in list_conscernee:
-			list_conscernee.remove(self)
+    def Kill(self):
+        """
+        Fonction qui tue un élément
+        """
+        self.vie = 0
 
-	def Afficher(self):
-		self.fenetre.blit(self.image, self.rect)
+    def Remove(self, list_conscernee):
+        """
+        Fonction qui supprime les éléments qui n'ont plus lieu d'être
+        """
+        if self.vie <= 0 and self in list_conscernee:
+            list_conscernee.remove(self)
+
+    def Afficher(self):
+        self.fenetre.blit(self.image, self.rect)
 
 
 class ElementGraphiqueAnimé(ElementGraphique):
-	"""
-	Animation des Elements Graphiques
-	"""
-	def __init__(self, x, y, img, fenetre):
-		super().__init__(x, y, img[0], fenetre)
-		self.images = img
-		self.timer = 0
-		self.numAnim = 0
+    """
+    Animation des Elements Graphiques
+    """
 
+    def __init__(self, x, y, img, fenetre):
+        super().__init__(x, y, img[0], fenetre)
+        self.images = img
+        self.timer = 0
+        self.numAnim = 0
 
-	def Afficher(self):
-		self.timer += 1
-		if self.timer > 10:
-			self.timer = 0
-			self.numAnim += 1
-			if self.numAnim >= len(self.images):
-				self.numAnim = 0
-			self.image = self.images[self.numAnim]
-		super().Afficher()
+    def Afficher(self):
+        self.timer += 1
+        if self.timer > 10:
+            self.timer = 0
+            self.numAnim += 1
+            if self.numAnim >= len(self.images):
+                self.numAnim = 0
+            self.image = self.images[self.numAnim]
+        super().Afficher()
 
 
 class ElementAnimeDir(ElementGraphiqueAnimé):
-	def __init__(self, x, y, images_all_dir, fenetre):
-		super().__init__(x, y, images_all_dir["Standing"], fenetre)
-		self.images_all_dir = images_all_dir
-		self.direction ="Standing"
-		self.old_direction="Standing"
+    def __init__(self, x, y, images_all_dir, fenetre):
+        super().__init__(x, y, images_all_dir["Standing"], fenetre)
+        self.images_all_dir = images_all_dir
+        self.direction = "Standing"
+        self.old_direction = "Standing"
 
+    def Afficher(self):
+        if self.old_direction != self.direction:
+            self.images = self.images_all_dir[self.direction]
+            self.numAnim = 0
+            self.old_direction = self.direction
 
-	def Afficher(self) :
-		if self.old_direction != self.direction:
-			self.images = self.images_all_dir[self.direction]
-			self.numAnim=0
-			self.old_direction = self.direction
-
-		super().Afficher()
+        super().Afficher()
 
 
 class Perso(ElementAnimeDir):
-	"""
-	Personnage qu'incarne le joueur
-	"""
-	def __init__(self, x, y, images_all_dir, fenetre, largeur, hauteur):
-		super().__init__(x, y, images_all_dir, fenetre)
-		self.type = "Perso"
-		self.rect.x = largeur // 2 - self.rect.w // 2
-		self.rect.y = hauteur - self.rect.h - 20
-		self.vie = 100
-		self.vitesse = 4
-		self.cooldown = 20
-		self.money = 0
-		self.kill = 0
+    """
+    Personnage qu'incarne le joueur
+    """
 
-	def Deplacer(self, touches, largeur):
-		self.direction = "Standing"
-		self.numAnim = 0
+    def __init__(self, x, y, images_all_dir, fenetre, largeur, hauteur):
+        super().__init__(x, y, images_all_dir, fenetre)
+        self.type = "Perso"
+        self.rect.x = largeur // 2 - self.rect.w // 2
+        self.rect.y = hauteur - self.rect.h - 20
+        self.vie = 100
+        self.vitesse = 4
+        self.cooldown = 20
+        self.money = 0
+        self.kill = 0
 
-		if touches[pygame.K_d] and self.rect.x <= largeur - self.rect.w:
-			self.rect.x += self.vitesse
-			self.direction = "Right"
+    def Deplacer(self, touches, largeur):
+        self.direction = "Standing"
+        self.numAnim = 0
 
-		if touches[pygame.K_a] and self.rect.x >= 0:
-			self.rect.x -= self.vitesse
-			self.direction = "Left"
+        if touches[pygame.K_d] and self.rect.x <= largeur - self.rect.w:
+            self.rect.x += self.vitesse
+            self.direction = "Right"
+
+        if touches[pygame.K_a] and self.rect.x >= 0:
+            self.rect.x -= self.vitesse
+            self.direction = "Left"
 
 # Second set game
 
-		if touches[pygame.K_RIGHT] and self.rect.x <= largeur - self.rect.w:
-			self.rect.x += self.vitesse
-			self.direction = "Right"
+        if touches[pygame.K_RIGHT] and self.rect.x <= largeur - self.rect.w:
+            self.rect.x += self.vitesse
+            self.direction = "Right"
 
-		if touches[pygame.K_LEFT] and self.rect.x >= 0:
-			self.rect.x -= self.vitesse
-			self.direction = "Left"
+        if touches[pygame.K_LEFT] and self.rect.x >= 0:
+            self.rect.x -= self.vitesse
+            self.direction = "Left"
 
-	def Tir(self, tirs, img, touches, i):
-		if touches[pygame.K_SPACE] and i%self.cooldown == 0:
-			tirs.append(Tir(self.rect.x - 12 + self.rect.w//2, self.rect.y - 30, img, self.fenetre, 5, 15))
+    def Tir(self, tirs, img, touches, i):
+        if touches[pygame.K_SPACE] and i % self.cooldown == 0:
+            tirs.append(Tir(self.rect.x - 12 + self.rect.w//2,
+                            self.rect.y - 30, img, self.fenetre, 5, 15))
 
-	def Bonus(self, bonus):
-		if self.colliderect(bonus):
-			if self.object == 'speed':
-				self.vitesse = 4*2
+    def Bonus(self, bonus):
+        if self.colliderect(bonus):
+            if self.object == 'speed':
+                self.vitesse = 4*2
 
-	def Alive(self):
-		if self.vie <= 0:
-			print("Perdu")
-
-
+    def Alive(self):
+        if self.vie <= 0:
+            print("Perdu")
 
 
 class Enemy(ElementGraphiqueAnimé):
-	"""
-	Ennemis animés arrivant en face du personnage
-	"""
-	def __init__(self, x, y, img, fenetre, pv, v, d, largeur, hauteur, _type):
-		super().__init__(x, y, img, fenetre)
-		self.vie = pv
-		self.vitesse = v
-		self.degats = d
-		self.t = 0
-		self.type = _type
-		self.trucy = randint(-10, 0)
-		self.truc2 = randint(10, largeur - 10)
-		self.Deplacer = self.DescenteLinéaire
+    """
+    Ennemis animés arrivant en face du personnage
+    """
 
-	def Choix(self, i):
-		"""
-		"""
-		if 1000 < i < 2500:
-			self.Deplacer = choice([self.DescenteLinéaire, self.DescenteEnCercles])
-		elif 2501 < i:
-			self.Deplacer = choice([self.DescenteLinéaire, self.DescenteEnCercles, self.DescenteSinusoïdale])
+    def __init__(self, x, y, img, fenetre, pv, v, d, largeur, hauteur, _type):
+        super().__init__(x, y, img, fenetre)
+        self.vie = pv
+        self.vitesse = v
+        self.degats = d
+        self.t = 0
+        self.type = _type
+        self.trucy = randint(-10, 0)
+        self.truc2 = randint(10, largeur - 10)
+        self.Deplacer = self.DescenteLinéaire
 
-	def DescenteLinéaire(self):
-		"""
-		Les ennemis descendent en ligne droite
-		"""
-		self.rect.y += self.vitesse
+    def Choix(self, i):
+        """
+        """
+        if 1000 < i < 2500:
+            self.Deplacer = choice(
+                [self.DescenteLinéaire, self.DescenteEnCercles])
+        elif 2501 < i:
+            self.Deplacer = choice(
+                [self.DescenteLinéaire, self.DescenteEnCercles, self.DescenteSinusoïdale])
 
-	def DescenteEnCercles(self):
-		"""
-		Les ennemis descendent en faisant des cercles de tailles différentes
-		"""
-		self.t += 1
-		self.rect.x = 50*cos(self.t/20) + self.truc2
-		self.rect.y = 50*sin(self.t/20) + self.trucy + self.t
+    def DescenteLinéaire(self):
+        """
+        Les ennemis descendent en ligne droite
+        """
+        self.rect.y += self.vitesse
 
-	def DescenteSinusoïdale(self):
-		"""
-		Les ennemis descendent en suivant des trajectoires sinusoîdales
-		"""
-		self.t += 1
-		self.rect.x = 200*cos(self.t/20) + 300
-		self.rect.y = self.t
-	
+    def DescenteEnCercles(self):
+        """
+        Les ennemis descendent en faisant des cercles de tailles différentes
+        """
+        self.t += 1
+        self.rect.x = 50*cos(self.t/20) + self.truc2
+        self.rect.y = 50*sin(self.t/20) + self.trucy + self.t
+
+    def DescenteSinusoïdale(self):
+        """
+        Les ennemis descendent en suivant des trajectoires sinusoîdales
+        """
+        self.t += 1
+        self.rect.x = 200*cos(self.t/20) + 300
+        self.rect.y = self.t
+
+
 class Tir(ElementGraphique):
-	"""
-	Tirs du personnage
-	"""
-	def __init__(self, x, y, img, fenetre, v, d):
-		super().__init__(x, y, img, fenetre)
-		self.type = "TirPerso"
-		self.vitesse = v
-		self.degats = d
-		self.vie = 1
+    """
+    Tirs du personnage
+    """
 
-	def Deplacer(self):
-		"""
-		Fonction qui gère le déplacement des tirs du Perso
-		"""
-		self.rect.y -= self.vitesse
+    def __init__(self, x, y, img, fenetre, v, d):
+        super().__init__(x, y, img, fenetre)
+        self.type = "TirPerso"
+        self.vitesse = v
+        self.degats = d
+        self.vie = 1
+
+    def Deplacer(self):
+        """
+        Fonction qui gère le déplacement des tirs du Perso
+        """
+        self.rect.y -= self.vitesse
+
 
 class Bonus(ElementGraphique):
-	def __init__(self, x, y, img, fenetre, bonus, time):
-		super(Bonus, self).__init__(img, fenetre, x,y)
-		self.vx = choice([-5, 5])
-		self.vy = choice([-5, 5])
-		self.type = 'bonus'
-		self.object = bonus
-		self.time = time
-	
-	def Deplacer(self, largeur, hauteur):
-		self.rect.x += self.vx
-		if self.rect.x > largeur - self.rect.w:
-			self.vx = -abs(self.vx)
-		if self.rect.x < 0:
-			self.vx = abs(self.vx)
+    def __init__(self, x, y, img, fenetre, bonus, time):
+        super(Bonus, self).__init__(img, fenetre, x, y)
+        self.vx = choice([-5, 5])
+        self.vy = choice([-5, 5])
+        self.type = 'bonus'
+        self.object = bonus
+        self.time = time
 
-		self.rect.y += self.vy
-		if self.rect.y > hauteur - self.rect.h :
-			self.vy = -abs(self.vy)
-		if self.rect.y < 0 :
-			self.vy = abs(self.vy)
+    def Deplacer(self, largeur, hauteur):
+        self.rect.x += self.vx
+        if self.rect.x > largeur - self.rect.w:
+            self.vx = -abs(self.vx)
+        if self.rect.x < 0:
+            self.vx = abs(self.vx)
 
-	def alive(self, time, tabBonus):
-		if time - self.time >= 250:
-			if self in tabBonus:
-				tabBonus.remove(self)
-		
+        self.rect.y += self.vy
+        if self.rect.y > hauteur - self.rect.h:
+            self.vy = -abs(self.vy)
+        if self.rect.y < 0:
+            self.vy = abs(self.vy)
+
+    def alive(self, time, tabBonus):
+        if time - self.time >= 250:
+            if self in tabBonus:
+                tabBonus.remove(self)
--- Shoot2.py
+++ Shoot2.py
@@ -1,6 +1,10 @@
 import pygame
-from Class import *
-from Fonctions import *
+# from Class import * || Mauvaise idée...  rend le débogage plus difficile car il embrombre l'espace des noms local, peux également entrainer des conflit avec les noms définie localement et (surtout) réduit la lisibilité, en important tout on ne sais pas concrétement ce qui est utilisé ou non ...
+from Class import ElementGraphique
+from Class import Perso 
+
+from Fonctions import * 
+
 
 pygame.init()
 
@@ -34,15 +38,15 @@ text_presentation = ElementGraphique(largeur / 2 - 240, 50, bank['text_menu'], f
 play_Bouton = ElementGraphique(largeur / 2 - 75, 150, bank['Play'], fenetre)
 option_Bouton = ElementGraphique(largeur / 2 - 75, 350, bank['Option'], fenetre)
 exit_Bouton = ElementGraphique(largeur / 2 - 75, 550, bank['Exit'], fenetre)
-pointeur1 = ElementGraphique(largeur / 2 - 200 , 150, bank['Pointeur'], fenetre)
-pointeur2 = ElementGraphique(largeur / 2 - 200 , 350, bank['Pointeur'], fenetre)
-pointeur3 = ElementGraphique(largeur / 2 - 200 , 550, bank['Pointeur'], fenetre)
+pointeur1 = ElementGraphique(largeur / 2 - 200, 150, bank['Pointeur'], fenetre)
+pointeur2 = ElementGraphique(largeur / 2 - 200, 350, bank['Pointeur'], fenetre)
+pointeur3 = ElementGraphique(largeur / 2 - 200, 550, bank['Pointeur'], fenetre)
 
 # init son
 son_menu = pygame.mixer.Sound("./Son Effect/Menu/Intro.wav")
 son_pointeur_menu = pygame.mixer.Sound("./Son Effect/Menu/pointeur.wav")
 
-son_menu.play() #Lancement du son_menu hors de la boucle, pour éviter le ralentissement sur le son
+son_menu.play()  # Lancement du son_menu hors de la boucle, pour éviter le ralentissement sur le son
 # ============= Menu =============
 
 # ============= Jeu =============
@@ -53,90 +57,89 @@ score = ElementGraphique(0, 0, bank["score"], fenetre)
 # ============= Jeu =============
 
 while continuer:
-	horloge.tick(60)
-	time += 1
-	touches = pygame.key.get_pressed()
+    horloge.tick(60)
+    time += 1
+    touches = pygame.key.get_pressed()
 
-	if touches[pygame.K_ESCAPE]:
-		continuer = False
+    if touches[pygame.K_ESCAPE]:
+        continuer = False
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			continuer = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
 
-	if state == "Menu":
-		menu_fond.Afficher()
-		text_presentation.Afficher()
-		play_Bouton.Afficher()
-		option_Bouton.Afficher()
-		exit_Bouton.Afficher()
-		horloge.tick(10) 
+    if state == "Menu":
+        menu_fond.Afficher()
+        text_presentation.Afficher()
+        play_Bouton.Afficher()
+        option_Bouton.Afficher()
+        exit_Bouton.Afficher()
+        horloge.tick(10)
 
-		selection_menu = move_Pointeur(selection_menu, touches, son_pointeur_menu)
+        selection_menu = move_Pointeur(selection_menu, touches, son_pointeur_menu)
 
-		if selection_menu == 1:
-			pointeur1.Afficher()
-			if touches[pygame.K_RETURN]:
-				son_menu.stop()
-				state = 'Jeu'
+        if selection_menu == 1:
+            pointeur1.Afficher()
+            if touches[pygame.K_RETURN]:
+                son_menu.stop()
+                state = 'Jeu'
 
-		if selection_menu == 2:
-			pointeur2.Afficher()
-			if touches[pygame.K_RETURN]:
-				state = 'Option'
+        if selection_menu == 2:
+            pointeur2.Afficher()
+            if touches[pygame.K_RETURN]:
+                state = 'Option'
 
-		if selection_menu == 3:
-			pointeur3.Afficher()
-			if touches[pygame.K_RETURN]:			
-				continuer = False
+        if selection_menu == 3:
+            pointeur3.Afficher()
+            if touches[pygame.K_RETURN]:
+                continuer = False
 
-	if state == "Jeu":
+    if state == "Jeu":
 
-		fondJeu.Afficher()
+        fondJeu.Afficher()
 
-		New_Bonus(time, tabBonus, bank, fenetre)
+        New_Bonus(time, tabBonus, bank, fenetre)
 
-		if time%cooldownEn == 0 and not boss:
-			New_Enemy(bank["enemys"], enemys, largeur, hauteur, fenetre, time)
+        if time % cooldownEn == 0 and not boss:
+            New_Enemy(bank["enemys"], enemys, largeur, hauteur, fenetre, time)
 
-		if time%cooldownBoss == 0:
-			boss = True
-			New_Boss(bank["boss"], enemys, largeur, hauteur, fenetre, time)
+        if time % cooldownBoss == 0:
+            boss = True
+            New_Boss(bank["boss"], enemys, largeur, hauteur, fenetre, time)
 
-		if len(enemys)>0:
-			boss = BossTimer(enemys[-1])
+        if len(enemys) > 0:
+            boss = BossTimer(enemys[-1])
 
-		for enemy in enemys:
-			enemy.Deplacer()
-			enemy.Afficher()
-			enemy.Collisions(perso)
-			for tir in tirsPerso:
-				enemy.Collisions(tir)
-			
-		for bonus in tabBonus :
-			bonus.Afficher()
-			bonus.Deplacer(largeur, hauteur)
-			bonus.alive(time, tabBonus)
+        for enemy in enemys:
+            enemy.Deplacer()
+            enemy.Afficher()
+            enemy.Collisions(perso)
+            for tir in tirsPerso:
+                enemy.Collisions(tir)
 
+        for bonus in tabBonus:
+            bonus.Afficher()
+            bonus.Deplacer(largeur, hauteur)
+            bonus.alive(time, tabBonus)
 
-		for tir in tirsPerso:   
-			tir.Afficher()
-			tir.Deplacer()
+        for tir in tirsPerso:
+            tir.Afficher()
+            tir.Deplacer()
 
-		perso.Afficher()
-		perso.Deplacer(touches, largeur)
-		perso.Tir(tirsPerso, bank["tirs"], touches, time)
+        perso.Afficher()
+        perso.Deplacer(touches, largeur)
+        perso.Tir(tirsPerso, bank["tirs"], touches, time)
 
-		x, enemys = SupprTrucs(enemys)
-		p, tirsPerso = SupprTrucs(tirsPerso)
+        x, enemys = SupprTrucs(enemys)
+        p, tirsPerso = SupprTrucs(tirsPerso)
 
-		perso.kill += x
+        perso.kill += x
 
-		bank["kill"] = font.render(str(perso.kill), 1, (255, 0, 0)).convert_alpha()
-		kill = ElementGraphique(70, 0, bank["kill"], fenetre)
-		score.Afficher()
-		kill.Afficher()
+        bank["kill"] = font.render(str(perso.kill), 1, (255, 0, 0)).convert_alpha()
+        kill = ElementGraphique(70, 0, bank["kill"], fenetre)
+        score.Afficher()
+        kill.Afficher()
 
-	pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
